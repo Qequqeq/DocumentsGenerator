c4eb7a91d752e0e5b390825664d7e19a8b52c055
Qequqeq <130912757+Qequqeq@users.noreply.github.com>
2026-02-22 20:10:51 +0300

    Add files via upload

--- storage.py
+++ storage.py
@@ -1,7 +1,9 @@
 from parser import parce_people_data, parce_org_data
+import json
+from pathlib import Path
 
 _JOBS = {}
-
+JOBS_DIR = Path("temp/jobs")
 
 def save_job(
     job_id,
@@ -24,6 +26,12 @@ def save_job(
         "org_data": org_data,
     }
 
+def save_job_data(job_id: str, job_data: dict) -> None:
+    file_path = JOBS_DIR / f"{job_id}.json"
+    file_path.parent.mkdir(parents=True, exist_ok=True)
+    with open(file_path, "w", encoding="utf-8") as f:
+        json.dump(job_data, f, ensure_ascii=False, indent=2, default=str)
+    _JOBS[job_id] = job_data
 
 def load_job(job_id):
     return _JOBS[job_id]
